c30b3c822170966a828ed9f45dd209ee325d5489
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2021-01-28 23:25:31 +0300

    provision download snap core, use --classic confinement for microk8s snap

--- microk8s/provision.sh
+++ microk8s/provision.sh
@@ -9,13 +9,17 @@ EOF
 
 chmod +x /etc/rc.local
 
+snap download --target-directory=/root core
 snap download --target-directory=/root microk8s
 
 cat > /usr/local/bin/launch.sh <<EOF
 #!/bin/bash
 
+snap ack /root/core_10583.assert
+snap install /root/core_10583.snap
+
 snap ack /root/microk8s_1910.assert
-snap install /root/microk8s_1910.snap
+snap install /root/microk8s_1910.snap --classic
 EOF
 
 chmod +x /usr/local/bin/launch.sh
